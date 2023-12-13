33a5982a145181f8def620dae8df802060df9655
amac81 <arnaldocanelas@gmail.com>
2023-12-13 14:02:39 +0000

    scripts.js update

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,41 @@
 const color = document.querySelector("#color");
 const ledStripHEXColor = document.querySelector("#ledStripHEXColor");
 const ledStripRGBColor = document.querySelector("#ledStripRGBColor");
-
-// global variables
-
+const pumpPowerSlider = document.querySelector("#pumpPowerSlider");
+const pumpPowerSliderValue = document.querySelector("#pumpPowerSliderValue");
+const brightnessSlider =  document.querySelector("#brightnessSlider");
+const brightnessSliderValue = document.querySelector("#brightnessSliderValue");
+const mistModeSlider =  document.querySelector("#mistModeSlider");
+const mistModeSliderValue = document.querySelector("#mistModeSliderValue");
 
 // Functions
 
-function updateSliderPWM(element) {
-  let motorSliderValue = document.getElementById("pwmMotorSlider").value;
-  document.getElementById("textMotorSliderValue").innerHTML = motorSliderValue;
+function XMLHttpRequestSend(path, value){
   let xhr = new XMLHttpRequest();
-  xhr.open("GET", "/motorslider?value="+motorSliderValue, true);
+  xhr.open("GET", path + value, true);
   xhr.send();
+}
+
+function updatePumpPower(value) {
+  pumpPowerSliderValue.innerHTML = value;
+  XMLHttpRequestSend("/pumppower?value=", value);
 }
 
 function updateRgbColor(value) {
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", "/ledstripcolor?value="+value, true);
-  xhr.send();
+  const rgbColor = hexToRgb(value);
+  ledStripHEXColor.innerHTML = value;
+  ledStripRGBColor.innerHTML = rgbColor.replaceAll("_", ", ");
+  XMLHttpRequestSend("/ledstripcolor?value=", rgbColor);
 }
 
-function updateBrigthness(element) {
-  let brightnessValue = document.getElementById("brightnessSlider").value;
-  document.getElementById("textBrightnessSliderValue").innerHTML = brightnessValue;
-  let xhr = new XMLHttpRequest();
+function updateBrigthness(value) {
+  brightnessSliderValue.innerHTML = value;
+  XMLHttpRequestSend("/brigthness?value=", value);
+}
 
-  xhr.open("GET", "/brigthness?value="+brightnessValue, true);
-  xhr.send();
+function updateMistMode(value) {
+  mistModeSliderValue.innerHTML = value;
+  XMLHttpRequestSend("/mistmode?value=", value);
 }
 
 function hexToRgb (hex) {
@@ -39,9 +47,17 @@ function hexToRgb (hex) {
 
 color.addEventListener("input", (e) => {
   const value = e.target.value;
-  const rgbColor = hexToRgb(value);
+  updateRgbColor(value);
+});
 
-  ledStripHEXColor.innerHTML = value;
-  ledStripRGBColor.innerHTML = rgbColor.replaceAll("_", ", ");
-  updateRgbColor(rgbColor);
+brightnessSlider.addEventListener("input", (e) => {
+  updateBrigthness(e.target.value);
+});
+
+pumpPowerSlider.addEventListener("input", (e) => {
+  updatePumpPower(e.target.value);
+});
+
+mistModeSlider.addEventListener("input", (e) => {
+  updateMistMode(e.target.value);
 });
